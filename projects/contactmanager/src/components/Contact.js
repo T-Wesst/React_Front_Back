7a83ab71abf4ff90e35837750e8524adc445d571
T-Wesst <code.daddy17@gmail.com>
2018-12-01 23:26:11 -0800

    destructor props

--- projects/contactmanager/src/components/Contact.js
+++ projects/contactmanager/src/components/Contact.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
+    const { name, phone, email } = this.props;
     return (
       <div>
         {/*class componets use this.props.<propName>*/}
-        <h4>{this.props.name}</h4>
+        <h4>{name}</h4>
         <ul>
-          <li>{this.props.email}</li>
-          <li>{this.props.phone}</li>
+          <li>Email: {email}</li>
+          <li>Phone: {phone}</li>
         </ul>
       </div>
     );
