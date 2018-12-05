6b0c2a5391b9509f0a82e0c1d677984823650ef3
T-Wesst <code.daddy17@gmail.com>
2018-12-04 20:02:49 -0800

    import Contacts component, set contact state and loop through each contact and output props

--- projects/contactmanager/src/App.js
+++ projects/contactmanager/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Contact from "./components/Contact";
+import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,16 +10,7 @@ class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
-          <Contact
-            name="John Doe"
-            email="jdoe@gmail.com"
-            phone="818-818-8188"
-          />
-          <Contact
-            name="Karren Smith"
-            email="karren@gmail.com"
-            phone="818-255-2121"
-          />
+          <Contacts />
         </div>
       </div>
     );
