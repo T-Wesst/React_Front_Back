a00a420f46d94393f6cb0c8b5c4a61bf7bcff712
T-Wesst <code.daddy17@gmail.com>
2018-12-04 20:05:18 -0800

    remove constructor, not needed to create state

--- projects/contactmanager/src/components/Contacts.js
+++ projects/contactmanager/src/components/Contacts.js
@@ -1,31 +1,28 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "John Doe",
-          email: "jdoe@gmail.com",
-          phone: "818-818-8118"
-        },
-        {
-          id: 2,
-          name: "Karen Smith",
-          email: "karen@gmail.com",
-          phone: "818-224-3234"
-        },
-        {
-          id: 3,
-          name: "Mike Myers",
-          email: "mike@gmail.com",
-          phone: "818-211-1122"
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "818-818-8118"
+      },
+      {
+        id: 2,
+        name: "Karen Smith",
+        email: "karen@gmail.com",
+        phone: "818-224-3234"
+      },
+      {
+        id: 3,
+        name: "Mike Myers",
+        email: "mike@gmail.com",
+        phone: "818-211-1122"
+      }
+    ]
+  };
   render() {
     const { contacts } = this.state;
     return (
