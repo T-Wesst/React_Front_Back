7a0ca19c11cf14f31e74e4acb3e1a8bf34589bae
T-Wesst <code.daddy17@gmail.com>
2018-12-04 21:55:51 -0800

    toggle update state

--- projects/contactmanager/src/components/Contact.js
+++ projects/contactmanager/src/components/Contact.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
+  onShowClick = e => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
   render() {
     const { name, phone, email } = this.props.contact;
     return (
       <div className="card card-body mb-3">
         {/*class componets use this.props.<propName>*/}
-        <h4>{name}</h4>
+        <h4>
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email: {email}</li>
           <li className="list-group-item">Phone: {phone}</li>
