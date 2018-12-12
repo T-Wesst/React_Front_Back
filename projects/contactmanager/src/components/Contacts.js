01ceb9887aa72cbd6d51f11d40265ea650e73a61
T-Wesst <code.daddy17@gmail.com>
2018-12-11 23:29:14 -0800

    return Consumer to access global state using value

--- projects/contactmanager/src/components/Contacts.js
+++ projects/contactmanager/src/components/Contacts.js
@@ -1,29 +1,7 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "818-818-8118"
-      },
-      {
-        id: 2,
-        name: "Karen Smith",
-        email: "karen@gmail.com",
-        phone: "818-224-3234"
-      },
-      {
-        id: 3,
-        name: "Mike Myers",
-        email: "mike@gmail.com",
-        phone: "818-211-1122"
-      }
-    ]
-  };
-
   deleteContact = id => {
     const { contacts } = this.state;
     const newContacts = contacts.filter(contact => contact.id !== id);
@@ -32,18 +10,24 @@ class Contacts extends Component {
     });
   };
   render() {
-    const { contacts } = this.state;
     return (
-      // React.fragment removes unneeded element
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            // React.fragment removes unneeded element
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+                />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
