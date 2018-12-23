3702dbf455791429fe9443fa8efb69061f32a48d
T-Wesst <code.daddy17@gmail.com>
2018-12-22 18:54:46 -0800

    create controlled component with onSubmit and onChange methods to change state and imported into App.js

--- projects/contactmanager/src/App.js
+++ projects/contactmanager/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/layout/Header';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from './context';
+import AddContact from './components/contacts/AddContact';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="App">
           <Header branding="Contact Manager" />
           <div className="container">
+            <AddContact />
             <Contacts />
           </div>
         </div>
--- projects/contactmanager/src/components/contacts/AddContact.js
+++ projects/contactmanager/src/components/contacts/AddContact.js
@@ -1,20 +1,69 @@
-// class based component
-//export addContact component
-// import addContact component into App.js above contacts component
-// add state to component as empty strings "name, email, phone"
-// parent div should have class of card mb-3
-// child div should contain card-header
-// sibling div should contain card-body
-// nested form
-// div form-group
-// label htmlFor="name"
-// input form-control form-control-lg placeholder name
-// input submit value add contact btn btn-block btn-light
-// (2) sibling for email and phone
-// destruct  name, email and phone from this.state
-// pass value attr input fields
-// when value is set on input = controlled component (needs onChangeHandler that will change state value for each key)
-// create onChangeHandler to update state for each key
-// set the state of name value
-// add onChange attr set to a function called this.onChange for input fields
-// create onSubmit method, prevent default, console.log state and add method as attr onto form
+import React, { Component } from 'react';
+
+class AddContact extends Component {
+  state = {
+    name: '',
+    email: '',
+    phone: ''
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+  render() {
+    const { name, email, phone } = this.state;
+    return (
+      <div className="card mb-3">
+        <div className="card-header">
+          {' '}
+          Add Contact
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  placeholder="enter name"
+                  className="form-control form-control-lg"
+                  name="name"
+                  value={name}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  placeholder="enter email"
+                  className="form-control form-control-lg"
+                  name="email"
+                  value={email}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="text"
+                  placeholder="enter phone"
+                  className="form-control form-control-lg"
+                  value={phone}
+                  name="phone"
+                  onChange={this.onChange}
+                />
+              </div>
+              <input
+                type="submit"
+                value="Add Contact"
+                className="btn btn-block btn-dark"
+                onSubmit={this.onSubmit}
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default AddContact;
