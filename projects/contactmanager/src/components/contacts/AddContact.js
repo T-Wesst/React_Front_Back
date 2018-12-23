e4fb77b06528137545f5fb936a4d85b213246a57
T-Wesst <code.daddy17@gmail.com>
2018-12-22 18:23:07 -0800

    controlled . omponent on change

---
+++ projects/contactmanager/src/components/contacts/AddContact.js
@@ -0,0 +1,20 @@
+// class based component
+//export addContact component
+// import addContact component into App.js above contacts component
+// add state to component as empty strings "name, email, phone"
+// parent div should have class of card mb-3
+// child div should contain card-header
+// sibling div should contain card-body
+// nested form
+// div form-group
+// label htmlFor="name"
+// input form-control form-control-lg placeholder name
+// input submit value add contact btn btn-block btn-light
+// (2) sibling for email and phone
+// destruct  name, email and phone from this.state
+// pass value attr input fields
+// when value is set on input = controlled component (needs onChangeHandler that will change state value for each key)
+// create onChangeHandler to update state for each key
+// set the state of name value
+// add onChange attr set to a function called this.onChange for input fields
+// create onSubmit method, prevent default, console.log state and add method as attr onto form
