0730566d811b9d5985c6cc87c7ab406a77c4baa0
T-Wesst <code.daddy17@gmail.com>
2018-12-23 11:51:23 -0800

    fetch placeholder data and render in test component

--- projects/contactmanager/src/components/test/Test.js
+++ projects/contactmanager/src/components/test/Test.js
@@ -2,41 +2,46 @@ import React, { Component } from 'react';
 
 class Test extends Component {
   state = {
-    test: 'test'
+    title: '',
+    body: ''
   };
 
   // usually where HTTP calls
   componentDidMount() {
-    console.log('componentDidMount...');
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(data => this.setState({ title: data.title, body: data.body }));
   }
-  // runs before did mount
-  componentWillMount() {
-    console.log('componentWillMount...');
-  }
-  // runs when component updates
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
-  //
-  componentWillUpdate() {
-    console.log('componentWillUpdate');
-  }
-  // when new props received usually with redux to turn piece of state into component prop
-  componentWillReceiveProps(nextProps, nextState) {
-    console.log('componentWillReceiveProps');
-  }
-  // replaces componentWillReceiveProps
-  static getDerivedStateFromProps(nextProps, prevState) {
-    return { test: 'something' }; // null or state
-  }
-  // before mutations are made eg: DOM
-  getSnapShotBeforeUpdate(prevProps, prevState) {
-    console.log('getSnapShotBeforeUpdate');
-  }
+  // // runs before did mount
+  // componentWillMount() {
+  //   console.log('componentWillMount...');
+  // }
+  // // runs when component updates
+  // componentDidUpdate() {
+  //   console.log('componentDidUpdate');
+  // }
+  // //
+  // componentWillUpdate() {
+  //   console.log('componentWillUpdate');
+  // }
+  // // when new props received usually with redux to turn piece of state into component prop
+  // componentWillReceiveProps(nextProps, nextState) {
+  //   console.log('componentWillReceiveProps');
+  // }
+  // // replaces componentWillReceiveProps
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   return { test: 'something' }; // null or state
+  // }
+  // // before mutations are made eg: DOM
+  // getSnapShotBeforeUpdate(prevProps, prevState) {
+  //   console.log('getSnapShotBeforeUpdate');
+  // }
   render() {
+    const { title, body } = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
